cb4161e716c5b28cf976fb1504729a805f789ce7
Ashley Ke <ashleyke200@gmail.com>
2019-04-11 10:52:46 -0700

    update wumpus to 2 steps away

--- src/Wumpus.java
+++ src/Wumpus.java
@@ -14,20 +14,35 @@ public class Wumpus extends Creature {
 
     @Override
     public void move(Graph.Node playerRoom) {
-        if(playerRoom.hasNeighbor(currentRoom.getName()) && currentRoom.getNumNeighbors() >= 1){ //if in adjacent room
-            if((currentRoom.getNumNeighbors()== 1 && currentRoom.getNeighbor(playerRoom.getName()) == null) || currentRoom.getNumNeighbors() > 1){ //if current has at least 2 neighbors or it has 1 neighbor which is not the playerRoom
+        if(playerRoom.isWithinTwoStepsOf(currentRoom.getName())) {
+            if (playerRoom.equals(currentRoom)) { //if in same room
+                if((currentRoom.getNumNeighbors()== 1 && currentRoom.getNeighbor(playerRoom.getName()) == null) || currentRoom.getNumNeighbors() > 1){
+                    Graph.Node nextRoom = currentRoom.getRandomNeighbor();
+                    while(nextRoom.equals(playerRoom)){
+                        nextRoom = currentRoom.getRandomNeighbor();
+                    }
+                    moveToRoom(playerRoom, currentRoom, nextRoom);
+                }
+            } else if(canMoveAwayFrom(playerRoom)) { //if one or two rooms away & can go to one that is >= 2 rooms away
                 Graph.Node nextRoom = currentRoom.getRandomNeighbor();
-                while(nextRoom.equals(playerRoom)){
+                while (!isValidRoom(nextRoom, playerRoom)) {
                     nextRoom = currentRoom.getRandomNeighbor();
                 }
                 moveToRoom(playerRoom, currentRoom, nextRoom);
             }
-        } else if(playerRoom.equals(currentRoom)){
-            Graph.Node nextRoom = currentRoom.getRandomNeighbor();
-            if(nextRoom != null){
-                moveToRoom(playerRoom, currentRoom, nextRoom);
-            }
         }
+    }
+
+    private boolean canMoveAwayFrom(Graph.Node playerRoom){
+        HashMap<String, Graph.Node> neighbors = currentRoom.getNeighborList();
+        for(String roomName : neighbors.keySet()){
+            if(!playerRoom.hasNeighbor(roomName) && !playerRoom.equals(neighbors.get(roomName))) return true;
+        }
+        return false;
+    }
+
+    private boolean isValidRoom(Graph.Node nextRoom, Graph.Node playerRoom) {
+        return !(nextRoom.equals(playerRoom) || playerRoom.hasNeighbor(nextRoom.getName()));
     }
 
 }
