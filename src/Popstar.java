5c11dff820e584897857bb84dd0bdebf5f99d26a
Ashley Ke <ashleyke200@gmail.com>
2019-04-11 11:09:07 -0700

    update popstar to move when 2 steps away

--- src/Popstar.java
+++ src/Popstar.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Popstar extends Creature {
     public Popstar(Graph.Node currentRoom){
         super(currentRoom);
@@ -11,9 +13,29 @@ public class Popstar extends Creature {
 
     @Override
     public void move(Graph.Node playerRoom){
-        if(currentRoom.hasNeighbor(playerRoom.getName())){
-            moveToRoom(playerRoom, currentRoom, playerRoom);
+        if(currentRoom.isWithinTwoStepsOf(playerRoom.getName())){
+            if(currentRoom.equals(playerRoom)){} //do nothing
+            else if(currentRoom.hasNeighbor(playerRoom.getName())){ //if one step away
+                moveToRoom(playerRoom, currentRoom, playerRoom);
+            } else{ //if two steps away
+                if(canMoveCloserTo(playerRoom)){
+                    Graph.Node nextRoom = currentRoom.getRandomNeighbor();
+                    while(!nextRoom.hasNeighbor(playerRoom.getName())){
+                        nextRoom = currentRoom.getRandomNeighbor();
+                    }
+                    moveToRoom(playerRoom, currentRoom, nextRoom);
+                }
+            }
         }
+    }
+
+    private boolean canMoveCloserTo(Graph.Node playerRoom) {
+        HashMap<String, Graph.Node> neighbors = currentRoom.getNeighborList();
+        for(String roomName : neighbors.keySet()){
+            Graph.Node room = neighbors.get(roomName);
+            if(room.hasNeighbor(playerRoom.getName())) return true;
+        }
+        return false;
     }
 
 }
